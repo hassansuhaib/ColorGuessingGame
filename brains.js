5b6d2fba3cebd3489482494d57c3613d5c69e380
hassansuhaib <ch.hassan.suhaib@gmail.com>
2020-03-27 18:11:09 +0500

    Today's Progres

--- brains.js
+++ brains.js
@@ -1,8 +1,10 @@
 var color = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 0, 255)", "rgb(0, 255, 255)", "rgb(255, 0, 255)"];
 var boxes = document.querySelectorAll(".color");
 var selectedMode = 6;
+var colorGuess = document.querySelector("#colorGuess");
 var randomSelection = randomChoiceGenerator(selectedMode);
 var choice = color[randomSelection];
+colorGuess.textContent = choice;
 
 
 for(let i = 0; i < boxes.length; i++){
